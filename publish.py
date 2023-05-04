f5e29bb1998ad950717fb1c362ed6e132a6da093
Quimzy <bam.boy2020@gmail.com>
2023-05-05 01:06:28 +0300

    Convert class works like a charm

--- publish.py
+++ publish.py
@@ -1,42 +1,62 @@
 import os, shutil
 import markdown
+from collections.abc import Iterable
 
 
 class Convert:
-    def __init__(self, dir="site"):
-        self.dir = dir
-         
-    def md2html(self, path):
-        with open(path, 'r') as f:
+    def __init__(self, src_dir="site", dist_dir="."):
+        self.src_dir = src_dir
+        self.dist_dir = dist_dir
+    
+    def flatten(self, arr):
+        lis = []
+        for item in arr:
+            if isinstance(item, Iterable) and not isinstance(item, str):
+                for x in self.flatten(item):
+                    lis.append(x) 
+            else:         
+                 lis.append(item)
+        
+        return lis
+
+    def replace_md(self, f): return os.path.splitext(f)[0]
+    def compare_content(self):
+        filter_dir = lambda d: self.src_dir not in d and '.' not in d
+        filter_file = lambda f: f[0] != '.' and any(ext in f for ext in ['html', 'md', 'markdown'])
+
+        site_cc = self.flatten([[self.src_dir+'/'+d for d in list(filter(lambda d: '.' not in d, dirs))] + [os.path.join(subdir, f) for f in files] for subdir, dirs, files in os.walk(self.src_dir)])
+
+        dist_cc = [] #current content
+        for subdir, dirs, files in os.walk(self.dist_dir):
+            if filter_dir(subdir[2:]):
+                dist_cc.extend(self.flatten([self.dist_dir+'/'+d for d in list(filter(filter_dir, dirs))] + [os.path.join(subdir, os.path.splitext(f)[0]) for f in list(filter(filter_file, files))]))     
+        
+        return [c for c in site_cc if self.replace_md(c.replace('site/', './')) not in dist_cc] +\
+                [c for c in dist_cc if c.replace('./', 'site/') not in [self.replace_md(x) for x in site_cc]]
+    
+    def md2html(self, dist_path, src_path):
+        with open(src_path, 'r') as f:
             html = markdown.markdown(f.read())
-
-        new_path = path.split('/')[1:]
-
-        subdir = '.'
-        if len(new_path) > 1: 
-            subdir = os.path.join(*(new_path[:-1]))
-            if not os.path.isdir(subdir):
-                os.mkdir(subdir)
-
-        file = new_path[-1]
-        file = file.replace(file.split('.')[-1], 'html')
-        print(os.path.join(subdir, file))
-
-        with open(os.path.join(subdir, file), 'w') as f:
+        
+        with open(dist_path, 'w') as f:
             f.write(html)
 
     def convert(self):
-        for subdir, dirs, files in os.walk(self.dir):
-            if "." not in subdir[2:]:
-                self.cleanup(subdir)
-                for f in files:
-                    if any(ext in f for ext in ["md", "markdown"]):
-                       self.md2html(os.path.join(subdir, f)) 
+        dist_content = self.compare_content()
+        for content in dist_content:    
+            dist_path = self.replace_md(content.replace(f'{self.src_dir}/', ''))
+            if os.path.isdir(content):
+                os.mkdir(dist_path)
 
-    def cleanup(self, dir):
-        for subdir in list(filter(lambda x: os.path.isdir(x) and '.' not in x, os.listdir(dir))):
-                shutil.rmtree(subdir)
-            
+            elif not os.path.isdir(content) and os.path.isdir(dist_path): 
+                shutil.rmtree(dist_path)
+
+            elif os.path.isfile(content):
+                self.md2html(dist_path+'.html', content)
+
+            elif not os.path.isdir(content) and os.path.isfile(dist_path+'.html'): 
+                os.remove(dist_path+'.html')
+
 if __name__ == '__main__':
     convert = Convert()
     convert.convert()
